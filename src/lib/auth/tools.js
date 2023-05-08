552cad4f01f30bbcae09acfd498e6268675c510c
DianaBerte <dianaberte.go@gmail.com>
2023-05-08 10:08:13 +0200

    Creates tools.js

---
+++ src/lib/auth/tools.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+
+export const createAccessToken = payload =>
+    new Promise((resolve, reject) =>
+        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1 week" }, (err, token) => {
+            if (err) reject(err)
+            else resolve(token)
+        })
+    ) // input: payload; output: Promise resolving into token
+
+export const verifyAccessToken = token =>
+    new Promise((resolve, reject) =>
+        jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+            if (err) reject(err)
+            else resolve(payload)
+        })
+    ) // input: token; output: Promise resolving into original payload
