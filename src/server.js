38453c25de0d1b98b14eddb76e5ae5f47a26c60e
DianaBerte <dianaberte.go@gmail.com>
2023-05-09 18:56:30 +0200

    Imports cloudinary

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import gamesRouter from "./api/games/beginner/gameOne/index.js";
 import interGamesRouter from "./api/games/intermediate/index.js";
 import advGamesRouter from "./api/games/advanced/index.js";
 import usersRouter from "./api/users/index.js";
+import filesRouter from "./api/files/index.js";
 import { forbiddenErrorHandler, unauthorizedErrorHandler, badRequestHandler, notFoundHandler, genericErrorHandler } from "./errorHandlers.js";
 import googleStrategy from "./lib/auth/googleOauth.js";
 
@@ -20,6 +21,7 @@ expressServer.use(passport.initialize())
 //ENDPOINTS
 expressServer.use("/games", gamesRouter, interGamesRouter, advGamesRouter)
 expressServer.use("/users", usersRouter)
+expressServer.use("/files", filesRouter)
 
 //ERROR HANDLERS
 expressServer.use(badRequestHandler)
