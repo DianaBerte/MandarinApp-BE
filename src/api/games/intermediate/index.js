c199e454948dbb77097a5d91030bc7450747ef2f
DianaBerte <dianaberte.go@gmail.com>
2023-05-10 20:14:42 +0200

    Understood: it's either /intermediate OR /intermediate/second

--- src/api/games/intermediate/index.js
+++ src/api/games/intermediate/index.js
@@ -15,18 +15,7 @@ interGamesRouter.post("/intermediate", async (req, res, next) => {
     }
 })
 
-// interGamesRouter.post("/intermediate/second", async (req, res, next) => {
-//     try {
-//         const newGame = new InterGamesModel(req.body)
-//         const { _id } = await newGame.save()
-//         res.status(201).send({ _id })
-//         console.log("_id: ", _id)
-//     } catch (error) {
-//         next(error)
-//     }
-// })
-
-interGamesRouter.get("/intermediate", async (req, res, next) => {
+interGamesRouter.get("/intermediate/second", async (req, res, next) => {
     try {
         const games = await InterGamesModel.find()
         res.send(games)
@@ -34,15 +23,6 @@ interGamesRouter.get("/intermediate", async (req, res, next) => {
         next(error)
     }
 })
-
-// interGamesRouter.get("/intermediate/second", async (req, res, next) => {
-//     try {
-//         const games = await InterGamesModel.find()
-//         res.send(games)
-//     } catch (error) {
-//         next(error)
-//     }
-// })
 
 interGamesRouter.get("/intermediate/:id", async (req, res, next) => {
     try {
@@ -57,19 +37,6 @@ interGamesRouter.get("/intermediate/:id", async (req, res, next) => {
     }
 })
 
-// interGamesRouter.get("/intermediate/second/:id", async (req, res, next) => {
-//     try {
-//         const game = await InterGamesModel.findById(req.params.id);
-//         if (game) {
-//             res.send(game)
-//         } else {
-//             next(createHttpError(404, `Game with id ${req.params.id} not found!`))
-//         }
-//     } catch (error) {
-//         next(error)
-//     }
-// })
-
 interGamesRouter.put("/intermediate/:id", async (req, res, next) => {
     try {
         const updatedGame = await InterGamesModel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
@@ -83,19 +50,6 @@ interGamesRouter.put("/intermediate/:id", async (req, res, next) => {
     }
 })
 
-// interGamesRouter.put("/intermediate/second/:id", async (req, res, next) => {
-//     try {
-//         const updatedGame = await InterGamesModel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-//         if (updatedGame) {
-//             res.send(updatedGame)
-//         } else {
-//             next(createHttpError(404, `Game with id ${req.params.id} not found!`))
-//         }
-//     } catch (error) {
-//         next(error)
-//     }
-// })
-
 interGamesRouter.delete("/intermediate/:id", async (req, res, next) => {
     try {
         const deletedGame = await InterGamesModel.findByIdAndDelete(req.params.id)
@@ -108,18 +62,5 @@ interGamesRouter.delete("/intermediate/:id", async (req, res, next) => {
         next(error)
     }
 })
-
-// interGamesRouter.delete("/intermediate/second/:id", async (req, res, next) => {
-//     try {
-//         const deletedGame = await InterGamesModel.findByIdAndDelete(req.params.id)
-//         if (deletedGame) {
-//             res.status(204).send()
-//         } else {
-//             next(createHttpError(404, `Game with id ${req.params.id} not found!`))
-//         }
-//     } catch (error) {
-//         next(error)
-//     }
-// })
 
 export default interGamesRouter
