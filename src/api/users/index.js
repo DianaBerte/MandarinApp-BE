2cc2b7b49016ab2ad15ea46c8b1f23314901740b
DianaBerte <dianaberte.go@gmail.com>
2023-05-13 09:53:22 +0200

    users.post sends access token

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -26,7 +26,9 @@ usersRouter.post("/", async (req, res, next) => {
     try {
         const newUser = new UsersModel(req.body)
         const { _id } = await newUser.save()
-        res.status(201).send({ _id })
+        const payload = { _id: _id };
+        const accessToken = await createAccessToken(payload);
+        res.status(201).send({ user: newUser, accessToken: accessToken })
     } catch (error) {
         next(error)
     }
