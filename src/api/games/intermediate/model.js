5f842bc73a69832d9583153d55550c38e5c7194d
DianaBerte <dianaberte.go@gmail.com>
2023-07-06 19:14:38 +0200

    To shuffle games, I must redefine the games model: one model for text, one model for audio games

--- src/api/games/intermediate/model.js
+++ src/api/games/intermediate/model.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+// To shuffle games, this should be the model for intermediate text games; we're missing the model for the intermediate audio games.
+
 const { Schema, model } = mongoose;
 
 const questionSchema = new Schema({
